85a5456c5b49339fb055a3c9b3f6f4c75b7c6eb6
Akash <akash>
2022-06-24 07:35:39 +0530

    done

---
+++ Practice.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Practice {
+	public static void main(String args[]) {
+		System.out.println("Running system program with jenkin "+new Date());
+		System.out.println("Running java program for Batch8");
+		System.out.println("Running this application again");
+		
+	}
+}
